87e55f8551b322d7d60dcbdeca816a321c5a277b
rkyzk <reikoyazaki@yahoo.com>
2023-02-12 13:49:09 +0000

    write SQL commands in sql-psycopg2.py

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -6,5 +6,18 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
+# Query 1 - select all records from the "Artist" table
+cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [235])
+
 # fetch the results (multiple)
-results = 
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
