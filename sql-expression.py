2add3715ba04132b00ba888655338c2de27ec0c4
rkyzk <reikoyazaki@yahoo.com>
2023-02-13 14:16:34 +0000

    add sql expression language in sql-exp.py

--- sql-expression.py
+++ sql-expression.py
@@ -3,8 +3,50 @@ from sqlalchemy import (
 )
 
 # executing the instructions from our localhost "chinook" database
-db = create_engine("postgres:///chinook")
+db = create_engine("postgresql:///chinook")
 meta = MetaData(db)
+
+# create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+
+# create variable for "Album" table
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+
+# create variable for "Track" table
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+)
+
 
 # making the connection
 with db.connect() as connection:
+    
+    # Query 1 select all records from "Artist"
+    # select_query = artist_table.select()
+
+    # Query 2 select only Name from "Artist"
+    select_query = track_table.select().where(track_table.c.AlbumId == 36)
+
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
